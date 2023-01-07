0a42c9896da9b812fd3f00e340fad6cd6b11a9a7
d3j1x <d3j1x@yahoo.com>
2023-01-07 17:02:18 +0100

    token

---
+++ WEB3JAVASCRIPT/StartUp/04 token/token/src/App.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react';
+import {ethers} from 'ethers';
+import Token from './artifacts/contracts/Token.sol/Token.json';
+
+import './App.css';
+
+const tokenAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
+
+function App() {
+
+  const [balance, setBalance] = useState();
+
+  useEffect(() => {
+    getBalance();
+  }, [])
+
+  async function getBalance(){
+    if(typeof window.ethereum !== 'undefined') {
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+      const balance = await contract.balanceOf(accounts[0]);
+      setBalance(balance / 10**18);
+    }
+  }
+  return (
+    <div className="App">
+
+      <p>Vous avez {balance} DNR</p>
+      
+    </div>
+  );
+}
+
+export default App;
